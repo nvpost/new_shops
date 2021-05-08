600578dbef563833ef1e776b66d60f7bf5727e97
nvpost <nvbpost@yandex.ru>
2021-05-08 16:12:36 +0300

    's'

--- sql/sql_data_pass.php
+++ sql/sql_data_pass.php
@@ -1,6 +1,5 @@
 <?php
 $db = new PDO('mysql:host=localhost;dbname=sportforlive;charset=UTF8', 'root', 'mysql');
-//$db = new PDO('mysql:host=localhost;dbname=co38747_tourist;charset=UTF8', 'co38747_tourist', 'mR9FYizA');
 
 
 
